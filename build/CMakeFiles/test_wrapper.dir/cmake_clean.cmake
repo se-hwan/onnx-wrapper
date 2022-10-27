c0bc74c7afc8a29aa21f1a89e142f94ca71c238f
se-hwan <sehwan@mit.edu>
2022-10-27 10:41:36 -0400

    init push

---
+++ build/CMakeFiles/test_wrapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_wrapper.dir/test.cpp.o"
+  "test_wrapper.pdb"
+  "test_wrapper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_wrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test.cpp
@@ -0,0 +1,8 @@
+// #include <onnxruntime_cxx_api.h>
+
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::cout << "Hello world!" << "\n";
+    return 0;
+}
